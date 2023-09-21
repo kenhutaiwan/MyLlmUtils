aed83fd8c322d39b80e690a7278140000f778d97
Ken Hu <ken.hu@hwacom.com>
2023-09-21 21:42:17 +0800

    add download_url; update README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # MyLlmUtils
 Personal utility package to make using LLM more easily
+
+see [How to upload your python package to PyPi](https://medium.com/@joel.barmettler/how-to-upload-your-python-package-to-pypi-65edc5fe9c56)
+
+```
+python3 setup.py sdist
+python3 setup.py clean --all
+```
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Ken Hu',                   # Type in your name
   author_email = 'ken.hu@hwacom.com',      # Type in your E-Mail
   url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/kenhutaiwan/MyLlmUtils/archive/refs/tags/0.1.tar.gz',    # I explain this later on
   keywords = ['LLM', 'OpenAI', 'Azure'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
         'langchain',
